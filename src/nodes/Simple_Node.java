3cc6bb0de90b3d56b299649c6acb3e35f75b1df1
Francisco Suarez <39706929+Yosoyfr@users.noreply.github.com>
2020-04-14 13:30:17 -0600

    Implementacion de la lista simplemente enlazada

---
+++ src/nodes/Simple_Node.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nodes;
+
+/**
+ *
+ * @author Francisco Suarez
+ */
+public class Simple_Node {
+	//El dato que contendra el nodo
+	Object data;
+	//Enlace siguiente del nodo
+	Simple_Node next;
+
+	//Constructor vacio del nodo
+	public Simple_Node() {
+	}
+
+	public Simple_Node(Object data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	//Accesores y modificadores de los atributos del nodo
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Simple_Node getNext() {
+		return next;
+	}
+
+	public void setNext(Simple_Node next) {
+		this.next = next;
+	}
+}
